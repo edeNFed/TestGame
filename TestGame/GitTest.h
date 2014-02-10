6c0c63b34ab7578bd1a2a637aad999757793aed2
Eden Federman <edenf2k@gmail.com>
2014-02-10 21:02:17 +0200

    בדיקה ראשונה

---
+++ TestGame/GitTest.h
@@ -0,0 +1,13 @@
+//
+//  GitTest.h
+//  TestGame
+//
+//  Created by Eden Federman on 2/10/14.
+//  Copyright (c) 2014 Eden Federman. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GitTest : NSObject
+
+@end
---
+++ TestGame/GitTest.m
@@ -0,0 +1,13 @@
+//
+//  GitTest.m
+//  TestGame
+//
+//  Created by Eden Federman on 2/10/14.
+//  Copyright (c) 2014 Eden Federman. All rights reserved.
+//
+
+#import "GitTest.h"
+
+@implementation GitTest
+
+@end
