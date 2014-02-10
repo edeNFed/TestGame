0f733b542cf1c0e84594602cfcb75be31efe7113
Eden Federman <edenf2k@gmail.com>
2014-02-10 20:51:23 +0200

    Initial commit from local git

---
+++ TestGame/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TestGame
+//
+//  Created by Eden Federman on 2/10/14.
+//  Copyright (c) 2014 Eden Federman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
